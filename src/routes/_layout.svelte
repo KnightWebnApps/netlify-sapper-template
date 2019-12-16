b713cbc2d76d592f33134ae0617a139e4257a95e
Derek <dknig43@gmail.com>
2019-12-16 10:16:01 -0500

    Init Commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		background-color: white;
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
